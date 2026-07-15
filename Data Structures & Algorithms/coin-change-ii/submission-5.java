56aed06a0325a22b2ff85fe96b17a75a7a91ec95
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-07-15 11:31:10 -0700

    Add: coin-change-ii - submission-5

---
+++ Data Structures & Algorithms/coin-change-ii/submission-5.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    Map<String, Integer> map = new HashMap<>();
+
+    public int change(int amount, int[] coins) {
+
+        Arrays.sort(coins);
+
+        return change(amount,coins,0);
+    }
+
+    public int change(int amount, int[] coins, int index) {
+        if(amount==0) {
+            return 1; // found a combination
+        }
+
+        if(amount<0 || coins.length==index) {
+            return 0;
+        }
+
+        String key = amount+"-"+index;
+
+        if(map.containsKey(key)) {
+            return map.get(key);
+        }
+
+        int coinAmount = coins[index];
+
+        if (coinAmount<=amount) {
+
+            map.put(key, change(amount-coinAmount,coins,index) + change(amount,coins,index+1));
+
+            return map.get(key);
+        } else {
+            return 0;
+        }
+    }
+}
