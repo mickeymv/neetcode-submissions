57de19c49d2781a90d9322da640fa6137fc5f80c
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-07-21 12:57:41 -0700

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int seniors=0;
+
+        for(String detail : details) {
+            int age = Integer.parseInt(detail.substring(11,13));
+            if(age>60) {
+                seniors++;
+            }
+        }
+
+        return seniors;
+    }
+}
