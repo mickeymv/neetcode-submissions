86f092c491a1e78e6f774d3ef0c0f579d93ea1bd
Mickey Vellukunnel <mickeymv@users.noreply.github.com>
2026-08-13 17:29:43 -0700

    Add: sort-colors - submission-5

---
+++ Data Structures & Algorithms/sort-colors/submission-5.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int l=0,r=nums.length-1;
+
+        int i=0;
+
+        while(i<=r) {
+            int num = nums[i];
+
+            if (num==0) { //swap with left pointer
+                nums[i]=nums[l];
+                nums[l]=0;
+                l++;
+            }
+
+            if (num==2) { //swap with right pointer
+                nums[i] = nums[r];
+                nums[r]=2;
+                r--;
+                continue;
+            }
+
+            i++;
+        }
+    }
+}
